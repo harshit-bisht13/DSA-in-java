c0c41f404d92ed09f5c060a5632da548a19942b8
Hgafayagagag <harshitbisht1325@gmail.com>
2026-08-25 23:19:34 +0530

    Solved sorting techniques

---
+++ Sorting techniques/BubbleSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public void sort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+            System.out.println(Arrays.toString(arr));
+        
+    }
+    public static void main(String[] args) {
+        BubbleSort b=new BubbleSort();
+        int[] arr={10,2,9,4,11};
+        b.sort(arr);
+    }
+}
---
+++ Sorting techniques/InsertionSort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr={9,8,7,6,5,4,3,2,1};
+        for(int i=1;i<arr.length;i++){
+            int j=i-1;
+            int key=arr[i];
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
